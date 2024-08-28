cd8860146c0477ccc78710c505e1fe669ce04587
LordofTriton <agboola918@gmail.com>
2024-08-28 21:44:14 +0100

    Fixed test issues.

--- test/tiktok.strategy.spec.ts
+++ test/tiktok.strategy.spec.ts
@@ -31,22 +31,22 @@ describe('TiktokStrategy', () => {
 
   it('should exchange code for token', async () => {
     const mockCode = 'mockCode';
-    const mockToken = 'mockToken';
-    mock.onPost(mockConfig.tokenEndpoint).reply(200, { access_token: mockToken });
+    const mockTokenResponse = { access_token: "mockNewToken" };
+    mock.onPost(mockConfig.tokenEndpoint).reply(200, mockTokenResponse);
 
     const response = await tiktokStrategy.exchangeCodeForToken(mockCode, 'redirect-uri');
     expect(response.status).toBe(true);
-    expect(response.data).toEqual(mockToken);
+    expect(response.data).toEqual(mockTokenResponse);
   });
 
   it('should refresh access token', async () => {
     const mockRefreshToken = 'mockRefreshToken';
-    const mockNewToken = 'mockNewToken';
-    mock.onPost(mockConfig.tokenEndpoint).reply(200, { access_token: mockNewToken });
+    const mockNewTokenResponse = { access_token: "mockNewToken" };
+    mock.onPost(mockConfig.tokenEndpoint).reply(200, mockNewTokenResponse);
 
     const response = await tiktokStrategy.refreshAccessToken(mockRefreshToken);
     expect(response.status).toBe(true);
-    expect(response.data).toEqual(mockNewToken);
+    expect(response.data).toEqual(mockNewTokenResponse);
   });
 
   it('should get user data', async () => {
