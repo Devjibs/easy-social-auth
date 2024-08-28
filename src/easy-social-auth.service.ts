7640f514008c85a1e7b97a1f00b18b8fa68e211c
LordofTriton <agboola918@gmail.com>
2024-08-28 14:09:31 +0100

    Added spotifyStrategy to SocialAuthService.

--- src/easy-social-auth.service.ts
+++ src/easy-social-auth.service.ts
@@ -2,15 +2,18 @@ import { GoogleStrategy } from './strategies/google.strategy';
 import { FacebookStrategy } from './strategies/facebook.strategy';
 import { TwitterStrategy } from './strategies/twitter.strategy';
 import { config } from './config';
+import { SpotifyStrategy } from './strategies/spotify.strategy';
 
 export class SocialAuthService {
   public googleStrategy?: GoogleStrategy;
   public facebookStrategy?: FacebookStrategy;
   public twitterStrategy?: TwitterStrategy;
+  public spotifyStrategy?: SpotifyStrategy;
 
   constructor() {
     if (config.google) this.googleStrategy = new GoogleStrategy(config.google);
     if (config.facebook) this.facebookStrategy = new FacebookStrategy(config.facebook);
     if (config.twitter) this.twitterStrategy = new TwitterStrategy(config.twitter);
+    if (config.spotify) this.spotifyStrategy = new TwitterStrategy(config.spotify);
   }
 }
